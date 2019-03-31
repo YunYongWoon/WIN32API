eecf335a7bd60b9c4530e029375f63523ed24493
Yun_Yongwoon <yuyun0124@gmail.com>
2019-03-31 17:09:57 +0900

    충돌체 선별 충돌 및 FPS계산

--- Win32Project/APIFrameWork/Include/Core/Timer.cpp
+++ Win32Project/APIFrameWork/Include/Core/Timer.cpp
@@ -9,14 +9,14 @@ CTimer::CTimer() : m_fTimeScale(1.f) {
 CTimer::~CTimer() {
 }
 
-bool CTimer::Init() {
+bool CTimer::Init(HWND hWnd) {
+	m_hWnd = hWnd;
 	QueryPerformanceFrequency(&m_tSecond);
 	QueryPerformanceCounter(&m_tTime);
 
 	m_fDeltaTime = 0.f;
 	m_fFPS = 0.f;
 	m_fFPSTime = 0.f;
-	m_iFrameMax = 60;
 	m_iFrame = 0;
 
 	return true;
@@ -28,4 +28,21 @@ void CTimer::Update() {
 
 	m_fDeltaTime = (tTime.QuadPart - m_tTime.QuadPart) / (float)m_tSecond.QuadPart;
 	m_tTime = tTime;
+
+	m_fFPSTime += m_fDeltaTime;
+	++m_iFrame;
+
+	if (m_fFPSTime >= 1.f) {
+		m_fFPS = m_iFrame / m_fFPSTime;
+		m_fFPSTime = 0.f;
+		m_iFrame = 0;
+#ifdef _DEBUG
+		char strFPS[64] = {};
+		sprintf_s(strFPS, "FPS : %.f\n", m_fFPS);
+		//_cprintf(strFPS);
+		SetWindowTextA(m_hWnd, strFPS);
+		OutputDebugStringA(strFPS);
+#endif // _DEBUG
+	}
+
 }
